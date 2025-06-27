414a85523622fce0bcdd9defa606ed69348ea0f5
Queenslay Jema <queenslayjema@gmail.com>
2025-06-27 09:30:55 +0300

    Feat:add signup

--- auth.ts
+++ auth.ts
@@ -1,8 +1,13 @@
 import api from "./axios.config";
 
-type LoginPayLoad = {
+type RegisterPayload = {
   phone: string;
   password: string;
+  username: string;
+  promo_code?: string;
+  reference?: string;
+  referer?: string;
+  campaign?: string;
 };
 
 type User = {
@@ -15,12 +20,49 @@ type User = {
   created_at: string;
 };
 
-export async function login({ phone, password }: LoginPayLoad): Promise<User> {
+export async function register({
+  phone,
+  password,
+  username,
+  promo_code,
+  reference,
+  referer,
+  campaign,
+}: RegisterPayload): Promise<User> {
+  try {
+    const res = await api.post("/auth/register", {
+      phone,
+      password,
+      username,
+      promo_code,
+      reference,
+      referer,
+      campaign,
+    });
+    const user: User = res.data;
+    return user;
+  } catch (err: any) {
+    if (err.response && err.response.data?.message) {
+      throw new Error(err.response.data.message);
+    }
+    throw new Error("Signup failed. Please try again.");
+  }
+}
+
+type LoginPayLoad = {
+  phone: string;
+  password: string;
+};
+
+export async function login({
+  phone,
+  password,
+}: LoginPayLoad): Promise<User> {
   try {
     const res = await api.post("/auth/login", { phone, password });
     const user: User = res.data;
     return user;
-  } catch (err) {
+  } catch (err: any) {
     if (err.response && err.response.data?.message) {
       throw new Error(err.response.data.message);
     }
