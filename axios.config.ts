a5813f9890c8c3a17bed5f36f806215ec883c70d
Queenslay Jema <queenslayjema@gmail.com>
2025-06-27 09:17:28 +0300

    Refactor: Remove request interceptor from axios configuration

--- axios.config.ts
+++ axios.config.ts
@@ -1,20 +1,9 @@
 import axios from "axios";
 
-export const TOKEN = "token";
-
 const api = axios.create({
   baseURL: "https://api.betkumi.com/auth",
   timeout: 50000,
+  withCredentials: true
 });
-
-api.interceptors.request.use(config => {
-  const newConfig = { ...config };
-  const authToken = localStorage.getItem(TOKEN);
-
-  if (newConfig.headers)
-    newConfig.headers.authorization = `Bearer ${authToken || ""}`;
-
-  return newConfig;
-}, error => Promise.reject(error));
 
 export default api;
